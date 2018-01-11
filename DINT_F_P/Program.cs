89c4810fa192d69166caf913d82d6f014eb8c0b3
javier moreno Vilaplana <javier.moreno.vilaplana@hotmail.com>
2018-01-11 13:17:57 +0100

    First Form

--- DINT_F_P/Program.cs
+++ DINT_F_P/Program.cs
@@ -16,7 +16,7 @@ namespace DINT_F_P
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TwitClientMain());
         }
     }
 }
---
+++ DINT_F_P/TwitClientMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DINT_F_P
+{
+    partial class TwitClientMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.textBoxContrasenya = new System.Windows.Forms.TextBox();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.TwittClient = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.labelUsuario);
+            this.panel1.Controls.Add(this.textBoxContrasenya);
+            this.panel1.Controls.Add(this.textBoxUsuario);
+            this.panel1.Controls.Add(this.TwittClient);
+            this.panel1.Location = new System.Drawing.Point(290, 129);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(367, 188);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.button1.Location = new System.Drawing.Point(146, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Location = new System.Drawing.Point(153, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Contraseña";
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.BackColor = System.Drawing.SystemColors.Control;
+            this.labelUsuario.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.labelUsuario.Location = new System.Drawing.Point(162, 44);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(43, 13);
+            this.labelUsuario.TabIndex = 3;
+            this.labelUsuario.Text = "Usuario";
+            // 
+            // textBoxContrasenya
+            // 
+            this.textBoxContrasenya.Location = new System.Drawing.Point(86, 102);
+            this.textBoxContrasenya.Name = "textBoxContrasenya";
+            this.textBoxContrasenya.Size = new System.Drawing.Size(195, 20);
+            this.textBoxContrasenya.TabIndex = 2;
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(86, 60);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(195, 20);
+            this.textBoxUsuario.TabIndex = 1;
+            // 
+            // TwittClient
+            // 
+            this.TwittClient.AutoSize = true;
+            this.TwittClient.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.TwittClient.Location = new System.Drawing.Point(155, 12);
+            this.TwittClient.Name = "TwittClient";
+            this.TwittClient.Size = new System.Drawing.Size(56, 13);
+            this.TwittClient.TabIndex = 0;
+            this.TwittClient.Text = "TwittClient";
+            // 
+            // TwitClientMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 472);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.Window;
+            this.Name = "TwitClientMain";
+            this.Text = "TwitClient";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.TextBox textBoxContrasenya;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.Label TwittClient;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
