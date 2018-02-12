57f459c62005c77f7b51415ca29497aba859ccc2
javier moreno Vilaplana <javier.moreno.vilaplana@hotmail.com>
2018-02-12 12:53:35 +0100

    added component cajatuit

---
+++ CajaTwitt/cajatwitt/UserControl1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace CajaTwitt
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl1));
+            this.pictureBoxFotoPerfil = new System.Windows.Forms.PictureBox();
+            this.richTextBoxCajaTuit = new System.Windows.Forms.RichTextBox();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.labelFavs = new System.Windows.Forms.Label();
+            this.labelRets = new System.Windows.Forms.Label();
+            this.pictureBoxRet = new System.Windows.Forms.PictureBox();
+            this.pictureBoxFav = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFotoPerfil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFav)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxFotoPerfil
+            // 
+            this.pictureBoxFotoPerfil.Location = new System.Drawing.Point(13, 12);
+            this.pictureBoxFotoPerfil.Name = "pictureBoxFotoPerfil";
+            this.pictureBoxFotoPerfil.Size = new System.Drawing.Size(100, 95);
+            this.pictureBoxFotoPerfil.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxFotoPerfil.TabIndex = 0;
+            this.pictureBoxFotoPerfil.TabStop = false;
+            this.pictureBoxFotoPerfil.Click += new System.EventHandler(this.pictureBoxFotoPerfil_Click);
+            // 
+            // richTextBoxCajaTuit
+            // 
+            this.richTextBoxCajaTuit.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxCajaTuit.Enabled = false;
+            this.richTextBoxCajaTuit.Location = new System.Drawing.Point(119, 28);
+            this.richTextBoxCajaTuit.Name = "richTextBoxCajaTuit";
+            this.richTextBoxCajaTuit.Size = new System.Drawing.Size(410, 70);
+            this.richTextBoxCajaTuit.TabIndex = 1;
+            this.richTextBoxCajaTuit.Text = "";
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Location = new System.Drawing.Point(120, 12);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(51, 13);
+            this.labelUser.TabIndex = 2;
+            this.labelUser.Text = "labelUser";
+            // 
+            // labelFavs
+            // 
+            this.labelFavs.AutoSize = true;
+            this.labelFavs.Location = new System.Drawing.Point(333, 101);
+            this.labelFavs.Name = "labelFavs";
+            this.labelFavs.Size = new System.Drawing.Size(35, 13);
+            this.labelFavs.TabIndex = 3;
+            this.labelFavs.Text = "label2";
+            // 
+            // labelRets
+            // 
+            this.labelRets.AutoSize = true;
+            this.labelRets.Location = new System.Drawing.Point(422, 101);
+            this.labelRets.Name = "labelRets";
+            this.labelRets.Size = new System.Drawing.Size(35, 13);
+            this.labelRets.TabIndex = 4;
+            this.labelRets.Text = "label3";
+            // 
+            // pictureBoxRet
+            // 
+            this.pictureBoxRet.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxRet.Image")));
+            this.pictureBoxRet.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxRet.InitialImage")));
+            this.pictureBoxRet.Location = new System.Drawing.Point(305, 98);
+            this.pictureBoxRet.Name = "pictureBoxRet";
+            this.pictureBoxRet.Size = new System.Drawing.Size(22, 20);
+            this.pictureBoxRet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxRet.TabIndex = 5;
+            this.pictureBoxRet.TabStop = false;
+            this.pictureBoxRet.Click += new System.EventHandler(this.pictureBoxFav_Click);
+            // 
+            // pictureBoxFav
+            // 
+            this.pictureBoxFav.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxFav.Image")));
+            this.pictureBoxFav.Location = new System.Drawing.Point(394, 98);
+            this.pictureBoxFav.Name = "pictureBoxFav";
+            this.pictureBoxFav.Size = new System.Drawing.Size(22, 20);
+            this.pictureBoxFav.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxFav.TabIndex = 6;
+            this.pictureBoxFav.TabStop = false;
+            this.pictureBoxFav.Click += new System.EventHandler(this.pictureBoxRet_Click);
+            // 
+            // UserControl1
+            // 
+            this.Controls.Add(this.pictureBoxFav);
+            this.Controls.Add(this.pictureBoxRet);
+            this.Controls.Add(this.labelRets);
+            this.Controls.Add(this.labelFavs);
+            this.Controls.Add(this.labelUser);
+            this.Controls.Add(this.richTextBoxCajaTuit);
+            this.Controls.Add(this.pictureBoxFotoPerfil);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(534, 121);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFotoPerfil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFav)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxFotoPerfil;
+        private System.Windows.Forms.RichTextBox richTextBoxCajaTuit;
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Label labelFavs;
+        private System.Windows.Forms.Label labelRets;
+        private System.Windows.Forms.PictureBox pictureBoxRet;
+        private System.Windows.Forms.PictureBox pictureBoxFav;
+    }
+}
---
+++ CajaTwitt/cajatwitt/UserControl1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CajaTwitt
+{
+
+    /// <summary>
+    /// Componente personalizado con el layout y el tratamiento de datos de cada tuit.
+    /// Contiene las funciones básicas de comportamiento.
+    /// </summary>
+    public partial class UserControl1: UserControl
+    {
+        public int Foto { get; set; }
+        public int Favs { get; set; }
+        public int Rets { get; set; }
+        public string Mensaje { get; set; }
+        public string User { get; set; }
+        public Image foto { get; set; }
+        public MySqlConnection conexion;
+
+        public UserControl1(ref MySqlConnection conex)
+        {
+
+            InitializeComponent();
+            conexion = conex;
+
+        }
+
+        /// <summary>
+        /// Suma uno a los favs.
+        /// Actualiza la BBDD.
+        /// </summary>
+        private void pictureBoxFav_Click(object sender, EventArgs e)
+        {
+            int favs = Int32.Parse(this.labelFavs.Text);
+            favs += 1;
+            this.labelFavs.Text = favs.ToString();
+            //TODO: y guardamos en base de datos
+        }
+
+        /// <summary>
+        /// Suma uno a los rets.
+        /// Actualiza la BBDD.
+        /// </summary>
+        private void pictureBoxRet_Click(object sender, EventArgs e)
+        {
+            int rets = Int32.Parse(this.labelRets.Text);
+            rets += 1;
+            this.labelRets.Text = rets.ToString();
+            //TODO: y guardamos en base de datos
+        }
+
+        /// <summary>
+        /// Setter de tuits para modificar desde fuera.
+        /// </summary>
+        public void SetTuit(string Mensaje) {
+
+            this.Mensaje = Mensaje;
+            richTextBoxCajaTuit.Text = Mensaje;
+        }
+
+        /// <summary>
+        /// Setter de nombre de usar para modificar desde fuera.
+        /// </summary>
+        public void SetUser(string User)
+        {
+            this.User = User;
+            labelUser.Text = User;
+
+        }
+
+        /// <summary>
+        /// Setter de favs para modificar desde fuera.
+        /// </summary>
+        public void SetFavs(int Favs) {
+
+            this.Favs = Favs;
+            labelFavs.Text = Favs.ToString();
+        }
+
+        /// <summary>
+        /// Setter de rets para modificar desde fuera.
+        /// </summary>
+        public void SetRets (int Rets)
+        {
+            this.Rets = Rets;
+            labelRets.Text = Rets.ToString();
+        }
+
+        /// <summary>
+        /// Setter de foto para modificar desde fuera.
+        /// </summary>
+        public void SetFoto(Image foto)
+        {
+            this.foto = foto;
+            pictureBoxFotoPerfil.Image = foto; ;
+        }
+
+        //goto_perfil
+        private void FotoPerfilReturnUsuario(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBoxFotoPerfil_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
